64364a77abdf69e41bdb81036144b757260ad46d
无名 <952532573@qq.com>
2016-05-17 09:09:19 +0800

    update

--- lianhanghao_crawler/spiders/lianhanghao.py
+++ lianhanghao_crawler/spiders/lianhanghao.py
@@ -23,7 +23,7 @@ class LianhanghaoSpider(scrapy.Spider):
     province_name_now = u"北京市"
     city_now = "35"
     city_name_now = u"北京市"
-    bank_list = [('2', '中国农业银行'), ('3', '中国银行'), ('4', '中国建设银行'), ('5', '国家开发银行'),
+    bank_list = [('1', '中国工商银行'), ('2', '中国农业银行'), ('3', '中国银行'), ('4', '中国建设银行'), ('5', '国家开发银行'),
                  ('6', '中国进出口银行'), ('7', '中国农业发展银行'), ('8', '交通银行'), ('9', '中国邮政储蓄银行'), ('10', '中信银行'),
                  ('11', '中国光大银行'), ('12', '华夏银行'), ('13', '中国民生银行'), ('14', '广东发展银行'), ('15', '深圳发展银行'),
                  ('16', '招商银行'), ('17', '兴业银行'), ('18', '上海浦东发展银行'), ('19', '城市商业银行'), ('20', '农村商业银行'),
@@ -38,7 +38,29 @@ class LianhanghaoSpider(scrapy.Spider):
                  ('62', '德国巴伐利亚州银行'), ('63', '瑞士信贷银行'), ('64', '加拿大蒙特利尔银行'), ('65', '澳大利亚和新西兰银行集团'),
                  ('66', '德富泰银行'), ('67', '厦门国际银行'), ('68', '法国巴黎银行（中国）'), ('69', '平安银行'), ('70', '青岛国际银行'),
                  ('71', '华一银行')]
-    # bank_list = [('1', '中国工商银行')]
+    banks2 = [('2', '中国农业银行')]
+    banks3 = [('3', '中国银行')]
+    banks4 = [('4', '中国建设银行')]
+    banks5 = [('5', '国家开发银行'), ('6', '中国进出口银行'), ('7', '中国农业发展银行')]
+    banks6 = [('8', '交通银行')]
+    banks7 = [('9', '中国邮政储蓄银行')]
+    banks8 = [('10', '中信银行')]
+    banks9 = [('11', '中国光大银行'), ('12', '华夏银行'), ('13', '中国民生银行'), ('14', '广东发展银行'), ('15', '深圳发展银行')]
+    banks10 = [('16', '招商银行')]
+    banks11 = [('17', '兴业银行')]
+    banks12 = [('18', '上海浦东发展银行'), ('19', '城市商业银行'), ('20', '农村商业银行'),
+               ('21', '恒丰银行'), ('22', '农村合作行'), ('23', '渤海银行'), ('24', '徽商银行'), ('25', '城市信用社'), ('26', '农村信用联社'),
+               ('27', '香港上海汇丰银行'), ('28', '东亚银行'), ('29', '南洋商业银行'), ('30', '恒生银行')]
+    banks13 = [('31', '中国银行（香港）'), ('32', '(香港地区)银行'), ('33', '集友银行'), ('34', '星展银行（香港）'), ('35', '永亨银行'),
+               ('36', '美国花旗银行'), ('37', '美国银行'), ('38', '美国摩根大通银行'), ('39', '日本三菱东京日联银行'), ('40', '日本日联银行'),
+               ('41', '日本三井住友银行'), ('42', '日本瑞穗实业银行'), ('43', '日本山口银行'), ('44', '韩国外换银行'), ('45', '韩国新韩银行'),
+               ('46', '韩国友利银行'), ('47', '韩国产业银行'), ('48', '韩国中小企业银行'), ('49', '新加坡星展银行'), ('50', '奥地利中央合作银行'),
+               ('51', '比利时联合银行'), ('52', '荷兰银行'), ('53', '荷兰商业银行'), ('54', '渣打银行'), ('55', '法国兴业银行'), ('56', '法国巴黎银行'),
+               ('57', '法国东方汇理银行'), ('58', '德国德累斯登银行'), ('59', '德意志银行'), ('60', '德国商业银行'), ('61', '德国西德银行'),
+               ('62', '德国巴伐利亚州银行'), ('63', '瑞士信贷银行'), ('64', '加拿大蒙特利尔银行'), ('65', '澳大利亚和新西兰银行集团'),
+               ('66', '德富泰银行'), ('67', '厦门国际银行'), ('68', '法国巴黎银行（中国）'), ('69', '平安银行'), ('70', '青岛国际银行'),
+               ('71', '华一银行')]
+    banks14 = []
 
     province_dict = {'4': '山西省', '24': '贵州省', '8': '黑龙江省', '10': '江苏省', '2': '天津市', '25': '云南省',
                      '34': '澳门', '14': '江西省', '33': '香港', '7': '吉林省', '32': '台湾', '12': '安徽省', '19': '广东省',
@@ -56,7 +78,7 @@ class LianhanghaoSpider(scrapy.Spider):
             yield self.parse_item(tr, **param)
 
         if self.first:
-            for bank_tuple in self.bank_list:
+            for bank_tuple in self.banks2:
                 self.first = False
                 for province, province_name in self.province_dict.iteritems():
                     city_list = self.get_city(province)
@@ -83,15 +105,15 @@ class LianhanghaoSpider(scrapy.Spider):
                     request = scrapy.Request('%s/%s' % (self.domain, page_link), callback=self.parse)
                     yield request
 
-        # 原 pages 控制逻辑
-        # pages = sel.xpath("//div[@class='pager']/a/@href").extract()
-        # print('pages: %s' % pages)
-        # if len(pages) > 2:
-        #     # 取倒数第2个 即"下一页"的链接
-        #     page_link = pages[-2]
-        #     print ('pages_link: %s' % page_link)
-        #     request = scrapy.Request('%s/%s' % ( self.domain, page_link), callback=self.parse)
-        #     yield request
+                    # 原 pages 控制逻辑
+                    # pages = sel.xpath("//div[@class='pager']/a/@href").extract()
+                    # print('pages: %s' % pages)
+                    # if len(pages) > 2:
+                    #     # 取倒数第2个 即"下一页"的链接
+                    #     page_link = pages[-2]
+                    #     print ('pages_link: %s' % page_link)
+                    #     request = scrapy.Request('%s/%s' % ( self.domain, page_link), callback=self.parse)
+                    #     yield request
 
     def parse_item(self, text, **kwargs):
         pass
@@ -149,7 +171,7 @@ class LianhanghaoSpider(scrapy.Spider):
         city = ''.join(ret['city'])
         city_name = ""
         province_code = ret['province']
-        city_list = self.get_city(province_code)
+        city_list = self.get_city("".join(province_code))
         for item in city_list:
             if item['id'] == city:
                 city_name = item['name']
